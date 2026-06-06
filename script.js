8893904faa40b83240d4bcd4c0018fb761b2d26e
Sameer Ali <140313541+devxsameer@users.noreply.github.com>
2026-06-06 17:56:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstWord(s) {
-  // your code here
+  s = s.trim();
+  const index = s.indexOf(" ");
+
+  if (index === -1) {
+    return s;
+  }
+
+  return s.slice(0, index);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
